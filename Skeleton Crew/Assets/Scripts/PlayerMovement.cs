0ccbd17dde419b312fcca62f401921286b94c3cb
Adam Ledet <adamcledet@gmail.com>
2025-08-22 19:14:35 -0500

    Testing old input system

--- Skeleton Crew/Assets/Scripts/PlayerMovement.cs
+++ Skeleton Crew/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,20 @@
+using JetBrains.Annotations;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Attributes
+    public float speed;
+    public InputAction moveUp;
+    public InputAction moveDown;
+    public InputAction moveLeft;
+    public InputAction moveRight;
+
+    public InputAction MoveAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +24,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            MovePlayer(new Vector3(0,speed,0));
+        }
+    }
 
+    public void MovePlayer(Vector3 direction)
+    {
+        Console.WriteLine(this.transform.position);
+        this.transform.position += direction;
     }
 }
