89df67c3df20036cde0f7526b7a8b4497c64668b
adamledet <44976275+adamledet@users.noreply.github.com>
2025-09-15 10:53:24 -0500

    Minor updates to movement script in Skeleton Crew
    Use input to determine direction vector and set 'moving'. If moving, move in direction an amount equal to speed.

--- Skeleton Crew/Assets/Scripts/PlayerMovement.cs
+++ Skeleton Crew/Assets/Scripts/PlayerMovement.cs
@@ -8,7 +8,8 @@ public class PlayerMovement : MonoBehaviour
 {
     //Attributes
     [SerializeField] float speed;
-    private Vector3 dir;
+    private Vector3 direction;
+    private bool moving;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,27 +20,33 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
+        moving = false;
+
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            MovePlayer(new Vector3(0,speed,0));
+            MovePlayer(new Vector3(0, 1, 0));
         }
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            MovePlayer(new Vector3(-speed,0,0));
+            MovePlayer(new Vector3(-1, 0, 0));
         }
-        if(Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            MovePlayer(new Vector3(0,-speed,0));
+            MovePlayer(new Vector3(0, -1, 0));
         }
-        if(Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            MovePlayer(new Vector3(0,0,-speed));
+            MovePlayer(new Vector3(0, 0, -1));
+        }
+
+        if (moving)
+        {
+            transform.position += (direction) * speed * Time.deltaTime;
         }
     }
 
-    public void MovePlayer(Vector3 direction)
+    public void MovePlayer(Vector3 dir)
     {
-        Console.WriteLine(this.transform.position);
-        this.transform.position += direction;
+        direction += dir;
     }
 }
